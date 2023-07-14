c2b122ea779899d2fe331368827e1f2fd7dd8080
SerhiiMaslov1979 <112109122+SerhiiMaslov1979@users.noreply.github.com>
2023-07-14 09:19:55 +0300

    1-2 fix

--- routes/api/users-register.js
+++ routes/api/users-register.js
@@ -4,43 +4,30 @@ const bcrypt = require("bcryptjs");
 
 const User = require("../../models/userModel");
 
-// POST /users/register
 router.post("/", async (req, res) => {
   try {
-    // Отримайте дані з запиту (email і password)
     const { email, password } = req.body;
 
-    // Виконайте валідацію даних
     if (!email || !password) {
       return res
         .status(400)
         .json({ message: "Email and password are required" });
     }
 
-    // Перевірте, чи вказаний email вже використовується
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(409).json({ message: "Email is already in use" });
     }
-
-    // Зашифруйте пароль перед збереженням в базі даних
     const hashedPassword = await bcrypt.hash(password, 10);
-
-    // Створіть нового користувача за даними, які пройшли валідацію
     const newUser = new User({
       email,
       password: hashedPassword,
     });
-
-    // Збережіть користувача в базі даних
     await newUser.save();
-
-    // Поверніть успішну відповідь з кодом 201
     res.status(201).json({
       user: { email: newUser.email, subscription: newUser.subscription },
     });
   } catch (error) {
-    // Обробіть помилки, якщо вони виникнуть
     res.status(500).json({ message: "Registration failed" });
   }
 });
