6bea7f3d4c44f4a5c0a6593931630dc7f1c4fbee
SerhiiMaslov1979 <112109122+SerhiiMaslov1979@users.noreply.github.com>
2023-07-25 21:03:42 +0300

    fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,139 +1,3 @@
-// const { User } = require("../models/userModel");
-// const multer = require("multer");
-// const path = require("path");
-
-// const upload = multer({
-//   storage: multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, "public/avatars");
-//     },
-//     filename: function (req, file, cb) {
-//       const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-//       const ext = path.extname(file.originalname);
-//       cb(null, uniqueSuffix + ext);
-//     },
-//   }),
-// });
-
-// const getCurrentUser = async (req, res) => {
-//   try {
-//     const userId = req.user._id;
-
-//     const user = await User.findById(userId);
-
-//     if (user) {
-//       res.json({
-//         email: user.email,
-//         subscription: user.subscription,
-//         avatarURL: user.avatarURL,
-//       });
-//     } else {
-//       res.status(401).json({ message: "Not authorized" });
-//     }
-//   } catch (error) {
-//     res.status(500).json({ message: "Internal Server Error" });
-//   }
-// };
-
-// const updateUserAvatar = async (req, res) => {
-//   try {
-//     if (!req.file) {
-//       return res.status(400).json({ message: "No avatar uploaded" });
-//     }
-
-//     const { filename } = req.file;
-
-//     req.user.avatarURL = `/avatars/${filename}`;
-//     await req.user.save();
-
-//     res.json({ avatarURL: req.user.avatarURL });
-//   } catch (error) {
-//     res.status(500).json({ message: "Internal Server Error" });
-//   }
-// };
-
-// module.exports = { getCurrentUser, updateUserAvatar, upload };
-// 2
-// const { User } = require("../models/userModel");
-// const multer = require("multer");
-// const path = require("path");
-// const jimp = require("jimp");
-// const fs = require("fs/promises");
-
-// const upload = multer({
-//   storage: multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, "tmp");
-//     },
-//     filename: function (req, file, cb) {
-//       const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-//       const ext = path.extname(file.originalname);
-//       cb(null, uniqueSuffix + ext);
-//     },
-//   }),
-// });
-
-// const getCurrentUser = async (req, res) => {
-//   try {
-//     const userId = req.user._id;
-
-//     const user = await User.findById(userId);
-
-//     if (user) {
-//       res.json({
-//         email: user.email,
-//         subscription: user.subscription,
-//         avatarURL: user.avatarURL,
-//       });
-//     } else {
-//       res.status(401).json({ message: "Not authorized" });
-//     }
-//   } catch (error) {
-//     res.status(500).json({ message: "Internal Server Error" });
-//   }
-// };
-
-// const updateUserAvatar = async (req, res) => {
-//   try {
-//     if (!req.file) {
-//       return res.status(400).json({ message: "No avatar uploaded" });
-//     }
-
-//     const { filename } = req.file;
-//     const avatarPath = path.join("tmp", filename); // Шлях до тимчасового файлу аватарки
-
-//     // Обробка зображення за допомогою пакету jimp
-//     const image = await jimp.read(avatarPath);
-//     await image.cover(250, 250);
-
-//     // Генерація унікального імені для аватарки та шлях до нового файлу
-//     const uniqueAvatarFilename = `${Date.now()}-${Math.round(
-//       Math.random() * 1e9
-//     )}.png`;
-//     const avatarDestination = path.join(
-//       "public",
-//       "avatars",
-//       uniqueAvatarFilename
-//     );
-
-//     // Збереження обробленої аватарки
-//     await image.writeAsync(avatarDestination);
-
-//     // Видалення тимчасового файлу
-//     await fs.unlink(avatarPath);
-
-//     // Збереження URL аватарки в поле avatarURL користувача
-//     req.user.avatarURL = `/avatars/${uniqueAvatarFilename}`;
-//     await req.user.save();
-
-//     res.json({ avatarURL: req.user.avatarURL });
-//   } catch (error) {
-//     res.status(500).json({ message: "Internal Server Error" });
-//   }
-// };
-
-// module.exports = { getCurrentUser, updateUserAvatar, upload };
-// 3
 const { User } = require("../models/userModel");
 const multer = require("multer");
 const path = require("path");
@@ -180,31 +44,25 @@ const updateUserAvatar = async (req, res) => {
     }
 
     const { filename } = req.file;
-    const avatarPath = path.join("tmp", filename); // Шлях до тимчасового файлу аватарки
+    const avatarPath = path.join("tmp", filename);
 
-    // Обробка зображення за допомогою пакету jimp
     const image = await jimp.read(avatarPath);
     await image.cover(250, 250);
 
-    // Генерація унікального імені для аватарки
     const uniqueAvatarFilename = `${Date.now()}-${Math.round(
       Math.random() * 1e9
     )}.png`;
 
-    // Шлях до нового файлу аватарки
     const avatarDestination = path.join(
       "public",
       "avatars",
       uniqueAvatarFilename
     );
 
-    // Збереження обробленої аватарки
     await image.writeAsync(avatarDestination);
 
-    // Видалення тимчасового файлу
     await fs.unlink(avatarPath);
 
-    // Збереження URL аватарки в поле avatarURL користувача
     req.user.avatarURL = `/avatars/${uniqueAvatarFilename}`;
     await req.user.save();
 
--- routes/api/avatarRoutes.js
+++ routes/api/avatarRoutes.js
@@ -1,33 +1,3 @@
-// const express = require("express");
-// const { updateUserAvatar } = require("../../controllers/userController");
-// const authMiddleware = require("../../middleware/authMiddleware");
-// const multer = require("multer");
-// const path = require("path");
-
-// const router = express.Router();
-
-// const upload = multer({
-//   storage: multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, "tmp"); // Зберігаємо тимчасово завантажену аватарку у папку tmp
-//     },
-//     filename: function (req, file, cb) {
-//       const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-//       const ext = path.extname(file.originalname);
-//       cb(null, uniqueSuffix + ext);
-//     },
-//   }),
-// });
-
-// router.patch(
-//   "/avatars",
-//   authMiddleware,
-//   upload.single("avatar"),
-//   updateUserAvatar
-// );
-
-// module.exports = router;
-
 const express = require("express");
 const { updateUserAvatar } = require("../../controllers/userController");
 const authMiddleware = require("../../middleware/authMiddleware");
@@ -39,7 +9,7 @@ const router = express.Router();
 const upload = multer({
   storage: multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "tmp"); // Зберігаємо тимчасово завантажену аватарку у папку tmp
+      cb(null, "tmp");
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
